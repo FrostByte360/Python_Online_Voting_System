94dc93a9db03bdf69ec38f85172fca336ad6a3cf
FrostByte360 <225485059+FrostByte360@users.noreply.github.com>
2026-06-12 14:05:58 +0800

    updated

---
+++ main.py
@@ -0,0 +1,71 @@
+import random
+
+from registerVoter import Register
+
+
+def optionDescription():
+    print("\n------------- VOTER'S REGISTRATION FORM -------------")
+    print("| Please fill in the necessary forms for registration.")
+
+# MAIN()
+# ======================================================================================================================
+
+while True:
+    print("\nOnline Voting System")
+    print("=============== ONLINE VOTING SYSTEM ===============")
+    print("[A] ========== Register Voter\n[B] ========== Cast Vote")
+    print("[C] ========== View Candidate List\n[D] ========== View Registered Voters List")
+    print("[E] ========== Exit Program")
+
+    option = input("| Please select from the following options: ").strip().upper()
+
+    match option:
+        case "A":
+            print(f"\nYou Selected: {option}")
+            optionDescription()
+            fname = input("First Name: ")
+            lname = input("Last Name: ")
+
+            randNum = random.randint(1,999)
+            randnumtostr = str(randNum)
+            letters = ['A','B','C','D','E','F','G','H','I','J','K',
+                       'L','M','N','O','P','R','S','T','U','V','W',
+                       'X','Y','Z']
+
+            randLettersA = random.choice(letters)
+            randLettersB = random.choice(letters)
+            randLettersC = random.choice(letters)
+            randLettersD = random.choice(letters)
+            randLettersE = random.choice(letters)
+            randLettersF = random.choice(letters)
+
+            id = (randnumtostr + randLettersA + "-" + randLettersB + randLettersC
+                  + randLettersD + "-" + randnumtostr + randLettersE + randLettersF)
+
+            voter = Register(fname, lname, id)
+            voter.showAndUpdateInfo()
+
+
+
+
+
+
+        case "B":
+            print("The feeling's quite bizarre")
+
+
+        case "C":
+            print('C')
+
+
+        case "D":
+            print('D')
+
+
+        case "E":
+            print('E')
+            break
+
+
+        case _:
+            print("Invalid input.")
--- registerVoter.py
+++ registerVoter.py
@@ -1,5 +1,39 @@
-class registerVoter():
-    def __init__(self, firstName, lastName, idnumber):
+class Register:
+    def __init__(self, firstName, lastName, voterID):
         self.firstName = firstName
         self.lastName = lastName
-        self.idnumber = idnumber
+        self.__voterID = voterID
+
+    def showAndUpdateInfo(self):
+        print(f"\nFirst Name: {self.firstName}")
+        print(f"Last Name: {self.lastName}")
+        print(f"Voter's Key/ID: {self.__voterID}")
+
+        # PRIVATE ACCESS | NOT FOR VIEWING
+        try:
+            path = "C:/Users/User/OneDrive/Documents/Programming/Python_Projects/Python_Online_Voting_System/pseudodata/voterslist.txt"
+
+            formatted_lines = []
+            formatted_lines.append(f"{self.firstName:<12} {self.lastName:<12} {self.__voterID:<6}")
+            with open(path, "a") as file:
+                for line in formatted_lines:
+                    file.write(line + "\n")
+        except FileNotFoundError:
+            print("Error: Database not found")
+
+        # PUBLIC ACCESS | ONLY FOR VIEWING
+        try:
+            path = "C:/Users/User/OneDrive/Documents/Programming/Python_Projects/Python_Online_Voting_System/pseudodata/voterslistPRIVATE.txt"
+
+            secretID = "****-***-*****"
+            formatted_lines = []
+            formatted_lines.append(f"{self.firstName:<12} {self.lastName:<12} {secretID:<6}")
+            with open(path, "a") as file:
+                for line in formatted_lines:
+                    file.write(line + "\n")
+
+            print("=============✔️=============")
+        except FileNotFoundError:
+            print("Error: Database not found")
+
+        print("| 🟩 Voter Registered Successfully")
